c0dbab2b993b577e2d2b6ba6cae025656f3ed379
Sneha Dharmalingam <sneha.dharma112@gmail.com>
2024-12-19 12:32:30 -0600

    commit everything

---
+++ mean-stack-example/client/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()), 
+    provideAnimationsAsync()
+  ]
+};
